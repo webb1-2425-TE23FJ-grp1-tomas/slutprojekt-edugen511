1d38b00a3042baf5673c978909e3fd4b5ff89519
edugen511 <edugen511@edu.linkoping.se>
2025-05-20 08:54:50 +0000

    ojnugårdetsnabbt

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -1,14 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const header = document.querySelector('.navmenu');
-    const toggleButton = document.getElementById('toggleHeaderBtn');
-  
-    toggleButton.addEventListener('click', function() {
-      if (header.style.display === 'none') {
-        header.style.display = 'grid';
-        toggleButton.textContent = 'Hide Header';
-      } else {
-        header.style.display = 'none';
-        toggleButton.textContent = 'Show Header';
-      }
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector(".navmenu");
+  const toggleButton = document.getElementById("toggleHeaderBtn");
+
+  toggleButton.addEventListener("click", function () {
+    if (header.style.display === "none") {
+      header.style.display = "grid";
+      toggleButton.textContent = "Hide Header";
+    } else {
+      header.style.display = "none";
+      toggleButton.textContent = "Show Header";
+    }
   });
+});
